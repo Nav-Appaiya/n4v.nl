b631e06287990c49d3fd477fd514a6f8a82338aa
Nav Appaiya <navarajh@gmail.com>
2017-07-23 18:07:44 +0200

    Final adjustments

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,7 +28,7 @@ jQuery(document).ready(function($) {
     $("#rss-feeds").rss(
     
         //Change this to your own rss feeds
-        "http://feeds.feedburner.com/TechCrunch/startups",
+        "http://feeds.feedburner.com/tutsplus/nav",
         
         {
         // how many entries do you want?
@@ -55,11 +55,11 @@ jQuery(document).ready(function($) {
     );
     
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    GitHubCalendar("#github-graph", "IonicaBizau");
+    GitHubCalendar("#github-graph", "Nav-Appaiya");
     
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "caseyscarborough", selector: "#ghfeed" });
+    GitHubActivity.feed({ username: "Nav-Appaiya", selector: "#ghfeed" });
 
 
 });
